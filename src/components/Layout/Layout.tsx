a755a089d0c093b2b01bf6b846b45b2f93724289
rs4spitfire_SRP-at-545477882052 <rs4spitfire@gmail.com>
2025-02-08 23:07:43 -0500

    GameBoard With 3x3 Cards Added

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
-import Header from './Header'
-
-
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import Header from './Header';
 
 // Declare LayoutProps to ensure TypeScript knows about children
 interface LayoutProps {
@@ -12,17 +10,15 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Header></Header>
+      <Header />
 
       {/* Main content area */}
-      <Box component="main" sx={{ flexGrow: 1 }}>
-        <Container maxWidth="lg" sx={{ mt: 4 }}>
+      <Box component="main" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-start' }}>
+        <Box sx={{ mt: 4, width: '1000px', m: 2.5 }}> {/* 20px margin */}
           {/* Place the child components or content here */}
           {children}
-        </Container>
+        </Box>
       </Box>
-
-
     </Box>
   );
 };
