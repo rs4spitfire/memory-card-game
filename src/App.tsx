2d52dc795a8b72aff1a417dc0ce2eeb7b6116c09
Mihir Jilka <mihirjilka12@gmail.com>
2025-02-11 13:23:02 -0500

    add home and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 import GameBoard from './components/GameBoard/GameBoard';
 
 const App: React.FC = () => {
   return (
-    <Layout>
-      <GameBoard />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<GameBoard />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
 
 export default App;
+
